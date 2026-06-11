57607704893a26ed511505837c01d206f6fe0df2
Hwi Kim <166241069+littlebit0@users.noreply.github.com>
2026-06-11 22:47:17 +0900

    [Passed] 세 실수의 반올림 (2ms, 0MB)

---
+++ trail0/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a,b,c;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    printf("%1.3f\n", a);
+    printf("%2.3f\n", b);
+    printf("%3.3f\n", c);
+    return 0;
+}
