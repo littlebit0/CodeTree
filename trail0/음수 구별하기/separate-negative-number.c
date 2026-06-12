36d4dbe675b7a29bff8f1305fba2ce3b6011a47a
Hwi Kim <166241069+littlebit0@users.noreply.github.com>
2026-06-12 22:00:33 +0900

    [Passed] 음수 구별하기 (2ms, 1MB)

---
+++ trail0/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    // Please write your code here.
+    printf("%d\n",n);
+    if (n < 0){
+        printf("minus");
+    }
+    return 0;
+}
